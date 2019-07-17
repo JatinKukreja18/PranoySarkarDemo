3001412e43173341e14b7008a35a0d8e4b996f4e
jatin kukreja <jatinkukreja18@yahoo.in>
2019-07-18 02:40:17 +0530

    changed pages

--- wp-content/themes/nowform-elegance/main.js
+++ wp-content/themes/nowform-elegance/main.js
@@ -1,13 +1,15 @@
 
 function makeImagesClickable(){
-    if(document.querySelector('.story-single')){
-        let storyImages = document.querySelectorAll('.story-single .entry-content img');
-        for (var i = 0; i < storyImages.length; i++) {
-            storyImages[i].addEventListener('click', function(event) {
-                console.log(this.currentSrc);		
-                document.querySelector('.ps-popup-image-modal').classList.add('visible');
-                document.querySelector("#ps-popup-image").src = this.currentSrc;
-            });
+    if(window.innerWidth < 768){
+        if(document.querySelector('.story-single')){
+            let storyImages = document.querySelectorAll('.story-single .entry-content img');
+            for (var i = 0; i < storyImages.length; i++) {
+                storyImages[i].addEventListener('click', function(event) {
+                    console.log(this.currentSrc);		
+                    document.querySelector('.ps-popup-image-modal').classList.add('visible');
+                    document.querySelector("#ps-popup-image").src = this.currentSrc;
+                });
+            }
         }
     }
 }
@@ -44,7 +46,6 @@ function fadeImages(){
 }
 
 function initialiseInputs(){
-    console.log(this);
     let inputs = document.querySelectorAll('.ps-contact-form input');
     for(let i = 0; i < inputs.length; i++){
         inputs[i].addEventListener('focus',function(){
@@ -54,6 +55,9 @@ function initialiseInputs(){
             this.parentElement.parentElement.classList.remove('focused');
             if(this.value.length != 0){
                 this.parentElement.parentElement.classList.add('touched');
+            }
+            else{   
+                this.parentElement.parentElement.classList.remove('touched');
             }
         })
         if(inputs[i].value.length != 0){
@@ -69,11 +73,22 @@ function initialiseInputs(){
             this.parentElement.parentElement.classList.remove('focused');
             if(this.value.length != 0){
                 this.parentElement.parentElement.classList.add('touched');
+            }else{
+                this.style.height = '32px';       
+                this.parentElement.parentElement.classList.remove('touched');
             }
         })
         if(textareas[i].value.length != 0){
             textareas[i].parentElement.parentElement.classList.add('touched');
+            textareas[i].style.minHeight = textareas[i].scrollHeight + 1 + 'px';          
         }
+        textareas[i].addEventListener('keyup',function(){
+            // this.style.height = '30px';      
+            if(this.scrollHeight < 200){
+                // this.style.height = '2px';          
+                this.style.minHeight = this.scrollHeight + 1 + 'px';          
+            } 
+        })
     }
     
 }
@@ -83,9 +98,25 @@ function toggleMenu(){
 }
 
 window.addEventListener('load', function() {
-    makeImagesClickable();
-})
-window.addEventListener('load', function() {
     fadeImages();
     initialiseInputs();
+    makeImagesClickable();
+})
+
+
+window.addEventListener('DOMContentLoaded', function() {
+    // fadeImagesSingles();
+    const images = document.querySelectorAll(".lazyloadme img");
+    if(images){
+        for(let i = 0; i < images.length; i++){
+                images[i].classList.add('lazy-load');
+        }
+    }
+    var myElements = document.querySelectorAll(".lazyloadme img");
+    lozad(myElements, { 
+        rootMargin: "0px 0px",
+        loaded: function (el) {
+            el.classList.add("is-loaded");
+        }
+    }).observe();
 })
